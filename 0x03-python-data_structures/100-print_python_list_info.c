ec8154291c038eb5a18b4e2de9e4bfc9cc21c410
abdallah <abdallah.saleh203@yahoo.com>
2023-09-11 11:10:40 -0700

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,13 +1,12 @@
-#include "Python.h"
-
+#include <Python.h>
 /**
- * print_python_list_info - Prints information about python objects
- * @p: PyObject pointer to print info about
- * Compile with:
- * gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared
+  * print_python_list_info - Prints information about python objects
+  * @p: PyObject pointer to print info about
+  * Compile with:
+  * gcc -Wall -Werror -Wextra -pedantic -std=c99 -shared
  (* -Wl,-soname,PyList -o libPyList.so -fPIC -I/usr/include/python3.4
  (* 100-print_python_list_info.c
-*/
+  */
 void print_python_list_info(PyObject *p)
 {
 	Py_ssize_t i, py_list_size;
