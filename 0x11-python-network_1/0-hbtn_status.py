10f577b3c3311582cf775b6f4befb3767565c642
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-01-07 19:42:45 +0200

    update

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
+from urllib import request
+
+if __name__ == "__main__":
+    with request.urlopen("https://intranet.hbtn.io/status") as response:
+        response = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(response)))
+        print("\t- content: {}".format(response))
+        print("\t- utf8 content: {}".format(response.decode(encoding='utf-8')))
